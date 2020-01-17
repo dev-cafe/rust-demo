83f29a000bde0ab16f181eb6af0b86614e8e12ca
Radovan Bast <bast@users.noreply.github.com>
2020-01-17 10:49:47 +0100

    generate random numbers from a seed

--- src/random.rs
+++ src/random.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{rngs::StdRng, Rng, SeedableRng};
 
 pub struct Point {
     pub x: f64,
@@ -6,7 +6,7 @@ pub struct Point {
 }
 
 pub fn random_points(num_points: i32) -> Vec<Point> {
-    let mut rng = rand::thread_rng();
+    let mut rng = StdRng::from_seed([0; 32]);
 
     let mut points = Vec::new();
 
